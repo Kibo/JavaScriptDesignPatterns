db9d997665a3aafcd39c6a5e46c77c3504b3b3b0
Tomas Jurman <tomasjurman@gmail.com>
2014-06-30 09:57:21 +0200

    v0.4

--- patterns/utils/utils.js
+++ patterns/utils/utils.js
@@ -1,3 +1,4 @@
+// ################################################
 /*
  * Shorthand function
  */
@@ -6,3 +7,55 @@ var $ = function( id ) {
 };
 
 $( 'content' ).getElementsByTagName( 'a' ); 
+
+// ################################################
+
+/**
+ * Checks null and undefined
+ * @param {Object} x
+ * @return {boolean}
+ */
+function existy(x) { 
+	return x != null; 
+};
+
+existy(null);
+//=> false
+
+existy(undefined);
+//=> false
+
+existy({}.notHere);
+//=> false
+
+existy((function(){})());
+//=> false
+
+existy(0);
+//=> true
+
+existy(false);
+//=> true
+
+// ################################################
+
+/**
+ * Determine if something should be considered as true
+ * @param {Object} x
+ * @return {boolean}
+ */
+function truthy(x) { 
+	return (x !== false) && existy(x);
+};
+
+truthy(false);
+//=> false
+
+truthy(undefined);
+//=> false
+
+truthy(0);
+//=> true
+
+truthy('');
+//=> true
